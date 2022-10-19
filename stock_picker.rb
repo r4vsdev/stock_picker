023066c84972b3af5264b9d6b099e7b2ec4534a2
r4vsdev <r4vsdev@gmail.com>
2022-10-19 16:35:06 -0300

    Solve the problem using math combinations

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,30 +1,16 @@
-require 'pry-byebug'
-prices = [17,3,6,9,15,8,6,1,10]
-
 def stock_picker(prices)
-  if prices.min == prices[-1]
-    prices.delete_at(-1)
+  result = []
+  max_profit = 0
+  prices.combination(2) do |combination|
+    profit = combination[1] - combination[0]
+    if profit > max_profit
+      max_profit = profit
+      result[0] = prices.index(combination[0])
+      result[1] = prices.index(combination[1])
+    end
   end
-  
-  best_buy   = prices.min
-  best_buy_i = prices.index(best_buy)
-  best_sell  = prices[best_buy_i..-1].max
-    
-  second_best_buy   = prices.min(2)[1]
-  second_best_buy_i = prices.index(second_best_buy)
-  second_best_sell  = prices[second_best_buy_i..-1].max
-
-  profit_1 = best_sell - best_buy
-  profit_2 = second_best_sell - second_best_buy
-
-  if profit_2 > profit_1
-    profit_1   = profit_2
-    best_buy   = prices.min(3)[2]
-    best_buy_i = prices.index(best_buy)
-    best_sell  = prices[best_buy_i..-1].max
-  end
-    result = [prices.index(best_buy), prices.index(best_sell)]
-  binding.pry
+  result
 end
 
+prices = [17,3,6,9,15,8,6,1,10]
 p stock_picker(prices)
