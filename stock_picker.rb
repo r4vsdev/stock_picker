ce08481f882170fd78ab1f462bd3a0592cedc764
r4vsdev <r4vsdev@gmail.com>
2022-10-19 16:40:08 -0300

    Change variable name for readability

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,15 +1,15 @@
 def stock_picker(prices)
-  result = []
+  days = []
   max_profit = 0
   prices.combination(2) do |combination|
     profit = combination[1] - combination[0]
     if profit > max_profit
       max_profit = profit
-      result[0] = prices.index(combination[0])
-      result[1] = prices.index(combination[1])
+      days[0] = prices.index(combination[0])
+      days[1] = prices.index(combination[1])
     end
   end
-  result
+  days
 end
 
 prices = [17,3,6,9,15,8,6,1,10]
