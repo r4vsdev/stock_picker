6087292b14dc71ed7db2647218847af5e32a9b09
r4vsdev <r4vsdev@gmail.com>
2023-02-16 20:35:27 -0300

    Add minor corrections

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 def stock_picker(prices)
   days = []
   max_profit = 0
   prices.combination(2) do |combination|
-    profit = combination[1] - combination[0]
+    profit = combination.last - combination.first
     if profit > max_profit
       max_profit = profit
-      days[0] = prices.index(combination[0])
-      days[1] = prices.index(combination[1])
+      days[0] = prices.index(combination.first)
+      days[1] = prices.index(combination.last)
     end
   end
   days
 end
 
-prices = [17,3,6,9,15,8,6,1,10]
-p stock_picker(prices)
+prices = [17, 3, 6, 9, 15, 8, 6, 1, 10]
+puts stock_picker(prices) # => [1,4]  # for a profit of $15 - $3 == $12
